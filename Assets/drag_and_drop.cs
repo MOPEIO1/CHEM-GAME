2c7d5c1b7d846d8cf5a96e853631df4caad7293e
Kingsley392 <superkingsley323@gmail.com>
2025-03-23 16:02:32 -0700

    new

--- Assets/drag_and_drop.cs
+++ Assets/drag_and_drop.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 public class drag_and_drop : MonoBehaviour
 {
     public GameObject Hydrogen;
+    public AudioSource popSound;
     Vector3 mousePosition;
+    public GameObject Explosion;
     private Vector3 GetMousePos(){
-	    return Camera.main.WorldToScreenPoint(transform.position);
+        return Camera.main.WorldToScreenPoint(transform.position);
     }
+
 
     private void OnMouseDown(){
-	    mousePosition = Input.mousePosition - GetMousePos();
+        mousePosition = Input.mousePosition - GetMousePos();
     }
+
 
     private void OnMouseDrag(){
-	    transform.position  = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+        transform.position  = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
     }
     private void OnCollisionEnter(Collision collision){
-        if (collision.gameObject.CompareTag("proton") && collision.gameObject.CompareTag("electron")){
-            Debug.Log("proton & electron");
-            Instantiate(Hydrogen, collision.contacts[0].point, Quaternion.identity);
+        if ( (this.tag == "proton" && collision.gameObject.CompareTag("electron") ) || (this.tag == "electron" && collision.gameObject.CompareTag("proton")) ){
+            if (gameObject.GetInstanceID() < collision.gameObject.GetInstanceID()){
+                Debug.Log("spawn hydrogen ");
+                Vector3 spawnPosition = collision.contacts[0].point;
+                Instantiate(Hydrogen, collision.contacts[0].point, Quaternion.identity);
+                if (Explosion != null)
+                {
+                    var copy = Instantiate(Explosion, spawnPosition, Quaternion.identity);
+                    popSound.Play();  
+                    Destroy(copy, 1.0f);
+                    // StartCoroutine(spawnExplosion(5.0f));
+                }
+                MeshRenderer mr = GetComponent<MeshRenderer>();
+                mr.enabled = false;
+                Destroy(gameObject, 5.0f );
+                Destroy(collision.gameObject);
+            }
         }
-
+    }
+    IEnumerator spawnExplosion(float duration)
+    {
+        Debug.Log("Before");
+        yield return new WaitForSeconds(duration);
     }
 
+
 }
+
