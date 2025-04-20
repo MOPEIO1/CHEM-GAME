ac9aafe2c9d10e80e760e97b20862932be8efb0e
mjcarbonell <maximcarbonell@gmail.com>
2025-04-20 14:14:38 -0700

    pot with spawner

---
+++ Assets/potScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potScript : MonoBehaviour
+{
+    public GameObject Helium; 
+    public Transform spawnPoint; 
+    public List<int> electronIDs = new List<int>(); 
+    public List<int> protonIDs = new List<int>(); 
+    public List<int> neutronIDs = new List<int>(); 
+    private bool hasSpawnedHelium = false; 
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("electron")) {
+            if (electronIDs.Contains(collision.gameObject.GetInstanceID())) {
+            }
+            else{
+                electronIDs.Add(collision.gameObject.GetInstanceID());
+            } 
+        }
+        if (collision.gameObject.CompareTag("proton")) {
+            if (protonIDs.Contains(collision.gameObject.GetInstanceID())) {
+            }
+            else{
+                protonIDs.Add(collision.gameObject.GetInstanceID());
+            } 
+        }
+        if (collision.gameObject.CompareTag("neutron")) {
+            if (neutronIDs.Contains(collision.gameObject.GetInstanceID())) {
+            }
+            else{
+                neutronIDs.Add(collision.gameObject.GetInstanceID());
+            } 
+        }
+    }
+    void Update(){
+        if ( (electronIDs.Count == 1 && protonIDs.Count == 1 && neutronIDs.Count == 1) && hasSpawnedHelium == false ){
+            spawnHelium(); 
+            hasSpawnedHelium = true; 
+        }
+    } 
+    private void spawnHelium(){
+        Debug.Log("spawn helium");
+        Vector3 spawnPosition = spawnPoint.position;
+        Instantiate(Helium, spawnPoint.position, Quaternion.identity);
+}
+}
