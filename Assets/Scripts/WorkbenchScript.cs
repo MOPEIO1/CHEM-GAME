b5de9cde772c246887af95a60c4b18e92e8978ce
mjcarbonell <maximcarbonell@gmail.com>
2025-05-11 14:54:00 -0700

    new

--- Assets/Scripts/WorkbenchScript.cs
+++ Assets/Scripts/WorkbenchScript.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 public class WorkbenchScript : MonoBehaviour{
     [SerializeField] private GameObject protonPrefab;
     [SerializeField] private GameObject electronPrefab;
+    [SerializeField] private GameObject neutronPrefab;
     [SerializeField] private Transform spawnPoint; 
 
     private void OnCollisionEnter(Collision collision){
         if (collision.gameObject.CompareTag("Rock")) {
             Destroy(collision.gameObject);
-            // Instantiate(electronPrefab, spawnPoint.position, Quaternion.identity);
-            int number = Random.Range(1, 4); // 0 to 99 inclusive
-            for(int i=0; i < number; i++){
-                int chance = Random.Range(0, 2); 
-                if (chance == 0){
-                    Instantiate(protonPrefab, spawnPoint.position, Quaternion.identity);
-                } else{
-                    Instantiate(electronPrefab, spawnPoint.position, Quaternion.identity);
-                }
-            }
-        } 
+            Instantiate(neutronPrefab, spawnPoint.position, Quaternion.identity);
+        }
     }
 }
